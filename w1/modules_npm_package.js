8b0658e8fa2a82044b9c7f2ed9fd25813a846a00
yair <yair999r@gmail.com>
2026-06-28 09:09:24 +0300

    add a dir

---
+++ w1/modules_npm_package.js
@@ -0,0 +1,64 @@
+//1
+
+// //CommonJS 
+// const fs = require("fs");
+// module.exports = { readFile };
+
+// //ESModules
+// import express from "express";
+// export default app;
+
+// //ESModules
+// export const PI = 3.14;
+
+// //CommonJS
+// const math = require("./math");
+
+
+//2
+
+// import { add } from "./math.js";
+
+// {
+//   "name": "myapp"
+// }
+
+// import express from "./express.js";
+
+// import fs from "./fs.js";
+// const path = ("./fs.js");
+
+//3
+
+//בוצע
+
+//4 
+
+//dependencies = express nodemon
+//devDependencies = jest mongoose eslint
+
+//5
+
+// 5.18.3
+//MAJOR = 5
+//MINOR = 18
+//PATCH = 3
+
+//6
+
+// "express": "^4.18.2" = update MINOR PATCH
+// "express": "~4.18.2" = update PATCH 
+// "express": "4.18.2" = no update
+// "express": "*" = update all - bad
+
+//7
+
+//1 = false
+//2 = true
+//3 = true
+//4 = false
+
+
+
+
+
